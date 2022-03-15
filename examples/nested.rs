a10aa24e81b7cb3d38a4b670c606e88d7b056584
Michael Hsu <myhsu@benjaminelectric.com>
2022-03-15 13:41:50 -0700

    stub in some more examples

---
+++ examples/nested.rs
@@ -0,0 +1,3 @@
+// todo add nested states example
+// It is possible to nest states.
+fn main() {}
---
+++ examples/with_plugins.rs
@@ -0,0 +1,3 @@
+// TODO: add example of states with other plugins
+// It is a common pattern in bevy to organize game code into separate plugins.
+fn main() {}
